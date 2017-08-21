c0fe6127741795056950d263d0ce644312a5fc68
MasWil <30875257+MasWil@users.noreply.github.com>
2017-08-21 09:08:01 -1000

    Math.floor/ Math.random
    Math.floor/ Math.random

---
+++ Math.js
@@ -0,0 +1,8 @@
+function wassup() {
+var randoNumber = (Math.floor(Math.random()*3));
+//Make a random number times the number of strings
+var favFoods = ["stupid", "smart", "dumb"];
+//Made three strings in array
+window.alert(favFoods[randoNumber]);
+//Showing the favFoods with the randoNumber integer//
+}
