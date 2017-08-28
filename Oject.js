d5936c8e886846a860e0f36507df233252bfc87c
MasWil <30875257+MasWil@users.noreply.github.com>
2017-08-28 08:45:09 -1000

    object
    object.literal

---
+++ Oject.js
@@ -0,0 +1,10 @@
+var newName = prompt("What is his new First Name?")
+
+var cole = {
+firstName:"Cole",
+lastName:"House",
+age: 15,
+grade:10
+}
+window.alert(cole.firstName);
+window.alert(cole.firstName  = newName);
