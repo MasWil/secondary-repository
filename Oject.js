26d510c9d13911619958f66f2117fa15f8da9dc4
MasWil <30875257+MasWil@users.noreply.github.com>
2017-08-30 08:30:48 -1000

    Update Oject.js
    Added array inside object

--- Oject.js
+++ Oject.js
@@ -8,3 +8,27 @@ grade:10
 }
 window.alert(cole.firstName);
 window.alert(cole.firstName  = newName);
+
+
+var Masen ={
+firstName:"Masen",
+age: 16, 
+grade: 11,
+friends:["lil boat", "connor Mcgregor", "yoda"]
+};
+var Willy ={
+firstName:"willy",
+age: 15, 
+grade: 10,
+friends:["jim", "joe", "jac"]
+};
+var jon ={
+firstName:"jon",
+age: 35, 
+grade: 12,
+friends:["Cleveland Brown", "Peter Griffy", ""]
+};
+
+var x =[Masen,Willy,jon];
+
+window.alert(x[1]friends[0]);
